e8f8249b43a3f430fae33127ae01db613f6c54ec
ANKIT KUMAR <knockout23486@gmail.com>
2026-08-21 22:34:18 +0530

    Update extension allowance

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 ];
 const corsOptions = {
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
+       if (!origin || allowedOrigins.includes(origin) || (origin && (origin.startsWith('chrome-extension://') || origin.startsWith('moz-extension://')))) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
